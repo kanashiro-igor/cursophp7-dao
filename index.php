1c3f4c5a9e2dd159a96465cf9f10244fcedf140d
igor.kanashiro <igor.kanashiro@aranao.local>
2021-03-03 10:59:52 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -8,10 +8,28 @@ $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);*/
 
-$root = new Usuario();
+// Carrega um usuário
+/*$root = new Usuario();
 
 $root->loadById(1);
 
-echo $root;
+echo $root;*/
+
+// Carrega uma lista de usuários
+/*$lista = Usuario::getList();
+
+echo json_encode($lista);*/
+
+// Carega uma lista de usuários buscando pelo login
+/*$search = Usuario::search("i");
+
+echo json_encode($search);*/
+
+// Carrega um usuário usando o login e senha
+$usuario = new Usuario();
+
+$usuario->login("Igor", "Senharius");
+
+echo $usuario;
 
 ?>
